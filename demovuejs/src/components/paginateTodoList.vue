1f830f11c97e98436a4d25cf3a3e89730a99367e
Nguyễn Thị Quỳnh Hương <iris.nguyen1309.qh@gmail.com>
2020-12-16 08:31:12 +0700

    Update components

---
+++ demovuejs/src/components/paginateTodoList.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col"><a href="#" @click="sort('title')">Title</a></th>
+        <th scope="col">
+          <a href="#" @click="sort('description')">Description</a>
+        </th>
+        <th scope="col"><a href="#" @click="sort('date')">Date</a></th>
+        <th scope="=col">Selection</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in paginate" :key="item.id">
+        <td
+          class="item-title"
+          @click="toggleTodo(item)"
+          :class="{
+            done: item.completed,
+          }"
+        >
+          {{ item.title }}
+        </td>
+        <td
+          class="item-desc"
+          @click="toggleTodo(item)"
+          :class="{
+            done: item.completed,
+          }"
+        >
+          {{ item.description }}
+        </td>
+        <td
+          class="item-date"
+          @click="toggleTodo(item)"
+          :class="{
+            done: item.completed,
+          }"
+        >
+          {{ item.date }}
+        </td>
+        <td>
+          <button type="button" class="btn btn-danger" @click="clickEdit(item)">
+            Edit
+          </button>
+          <button
+            type="button"
+            class="btn btn-success"
+            @click="clickDelete(item)"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+      <div class="page">
+      <ul>
+        <li v-for="pageNumber in totalPages" v-bind:key="pageNumber">
+          <a
+            v-bind:key="pageNumber"
+            href="#"
+            @click="setPage(pageNumber)"
+            :class="{
+              current: currentPage === pageNumber,
+              last:
+                pageNumber == totalPages &&
+                Math.abs(pageNumber - currentPage) > 3,
+              first: pageNumber == 1 && Math.abs(pageNumber - currentPage) > 3
+            }"
+            >{{ pageNumber }}</a
+          >
+        </li>
+      </ul>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 3,
+      resultCount: 2,
+    };
+  },
+  props: {
+    searchName: Array
+  },
+  computed: {
+    totalPages: function() {
+      return Math.ceil(this.resultCount / this.itemsPerPage);
+    },
+    paginate: function() {
+      let self = this;
+      if (
+        !this.searchName ||
+        this.searchName.length != this.searchName.length
+      ) {
+        return;
+      }
+      self.resultCount = this.searchName.length;
+      if (this.currentPage >= this.totalPages) {
+        self.currentPage = this.totalPages;
+      }
+      var index = this.currentPage * this.itemsPerPage - this.itemsPerPage;
+      return this.searchName.slice(index, index + this.itemsPerPage);
+    },
+  },
+  methods: {
+    setPage: function(pageNumber) {
+      this.currentPage = pageNumber;
+    },
+    sort: function(s) {
+      //if s == current sort, reverse
+      if (s === this.currentSort) {
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+        console.log(this.currentSortDir);
+      }
+      this.currentSort = s;
+    },
+    clickEdit(item) {
+      this.$emit("edit", item);
+    },
+    clickDelete(item) {
+      this.$emit("delete", item);
+    },
+    
+    toggleTodo(item) {
+      this.$emit("toggleTodo", item);
+    }
+  },
+};
+</script>
--- demovuejs/src/components/todoList.vue
+++ demovuejs/src/components/todoList.vue
@@ -25,90 +25,19 @@
           v-if="isEdit"
         ></editemployee>
       </div>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col"><a href="#" @click="sort('title')">Title</a></th>
-            <th scope="col">
-              <a href="#" @click="sort('description')">Description</a>
-            </th>
-            <th scope="col"><a href="#" @click="sort('date')">Date</a></th>
-            <th scope="=col">Selection</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in paginate" :key="item.id">
-            <td
-            class="item-title"
-              @click="toggleTodo(item)"
-              :class="{
-                done: item.completed
-              }"
-            >
-              {{ item.title }}
-            </td>
-            <td
-            class="item-desc"
-              @click="toggleTodo(item)"
-              :class="{
-                done: item.completed
-              }"
-            >
-              {{ item.description }}
-            </td>
-            <td
-            class="item-date"
-              @click="toggleTodo(item)"
-              :class="{
-                done: item.completed
-              }"
-            >
-              {{ item.date }}
-            </td>
-            <td>
-              <button
-                type="button"
-                class="btn btn-danger"
-                @click="clickEdit(item)"
-              >
-                Edit
-              </button>
-              <button
-                type="button"
-                class="btn btn-success"
-                @click="clickDelete(item)"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="page">
-      <ul>
-        <li v-for="pageNumber in totalPages" v-bind:key="pageNumber">
-          <a
-            v-bind:key="pageNumber"
-            href="#"
-            @click="setPage(pageNumber)"
-            :class="{
-              current: currentPage === pageNumber,
-              last:
-                pageNumber == totalPages &&
-                Math.abs(pageNumber - currentPage) > 3,
-              first: pageNumber == 1 && Math.abs(pageNumber - currentPage) > 3
-            }"
-            >{{ pageNumber }}</a
-          >
-        </li>
-      </ul>
-      </div>
+      <TodoListWithPagination
+        v-bind:searchName="searchName"
+        @edit="clickEdit"
+        @delete="clickDelete"
+        @toggleTodo="toggleTodo"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import editemployee from "../components/addNew";
+import TodoListWithPagination from "./paginateTodoList";
 export default {
   name: "TodoList",
   data() {
@@ -119,43 +48,36 @@ export default {
           title: "Do the dishes",
           description: "Bruschetta- famous appetizer in Italy",
           completed: false,
-          date: "Mon Nov 02 2020 17:59:00 GMT+0700 (Giờ Đông Dương)"
+          date: "Mon Nov 02 2020 17:59:00 GMT+0700 (Giờ Đông Dương)",
         },
         {
           id: 2,
           title: "Take out the trash",
           description: "I take out the garbage",
           completed: false,
-          date: "	Sun Nov 01 2020 17:59:00 GMT+0700 (Giờ Đông Dương)"
+          date: "	Sun Nov 01 2020 17:59:00 GMT+0700 (Giờ Đông Dương)",
         },
         {
           id: 3,
           title: "Finish doing laundry",
           description: "Therefore, you need to add water.",
           completed: false,
-          date: "	Wed Nov 04 2020 18:00:00 GMT+0700 (Giờ Đông Dương)"
-        }
+          date: "	Wed Nov 04 2020 18:00:00 GMT+0700 (Giờ Đông Dương)",
+        },
       ],
       employee: null,
       isEdit: false,
       search: "",
-      array: [],
-      cats: [],
       currentSort: "title",
       currentSortDir: "asc",
-      currentPage: 1,
-      itemsPerPage: 3,
-      resultCount: 2
     };
   },
   computed: {
     searchName: function() {
       return (
         this.sortedCats &&
-        this.list.filter(items => {
-          return (
-            items.title.toLowerCase().includes(this.search.toLowerCase())
-          );
+        this.list.filter((items) => {
+          return items.title.toLowerCase().includes(this.search.toLowerCase());
         })
       );
     },
@@ -169,27 +91,10 @@ export default {
         return 0;
       });
     },
-    totalPages: function() {
-      return Math.ceil(this.resultCount / this.itemsPerPage);
-    },
-    paginate: function() {
-       let self = this;
-      if (
-        !this.searchName ||
-        this.searchName.length != this.searchName.length
-      ) {
-        return;
-      }
-      self.resultCount = this.searchName.length;
-      if (this.currentPage >= this.totalPages) {
-     self.currentPage = this.totalPages;
-      }
-      var index = this.currentPage * this.itemsPerPage - this.itemsPerPage;
-      return this.searchName.slice(index, index + this.itemsPerPage);
-    }
   },
   components: {
-    editemployee: editemployee,
+    editemployee,
+    TodoListWithPagination,
   },
   methods: {
     clickAdd() {
@@ -198,13 +103,13 @@ export default {
         title: "",
         description: "",
         completed: false,
-        date: ""
+        date: "",
       };
       this.employee = employee;
       this.isEdit = true;
     },
     clickSave(employee) {
-      let index = this.list.findIndex(items => items.id == employee.id);
+      let index = this.list.findIndex((items) => items.id == employee.id);
       if (index >= 0) {
         this.list.splice(index, 1, employee);
       } else {
@@ -217,23 +122,13 @@ export default {
       this.isEdit = true;
     },
     clickDelete(employee) {
-      let index = this.list.findIndex(items => items.id == employee.id);
+      let index = this.list.findIndex((items) => items.id == employee.id);
       this.list.splice(index, 1);
     },
-    sort: function(s) {
-      //if s == current sort, reverse
-      if (s === this.currentSort) {
-        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
-        console.log(this.currentSortDir);
-      }
-      this.currentSort = s;
-    },
-    setPage: function(pageNumber) {
-      this.currentPage = pageNumber;
-    },
+
     toggleTodo(todo) {
       todo.completed = !todo.completed;
-    }
-  }
+    },
+  },
 };
 </script>
