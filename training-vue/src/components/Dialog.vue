26d451940a2c20b858fb521aaf7741fe1c29506f
tailieuweb <tailieuweb.com@gmail.com>
2021-07-05 14:40:55 +0700

    Dev Dialog

---
+++ training-vue/src/components/Dialog.vue
@@ -0,0 +1,175 @@
+<!-- https://getbootstrap.com/docs/4.0/components/modal/ -->
+<template>
+  <div>
+      <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <!-- Form element -->
+          <div class="modal-container">
+            <div class="modal-header">
+              <slot name="header">
+                default header
+              </slot>
+            </div>
+
+            <div class="modal-body">
+              <!-- Title -->
+              <div class="form-group text-left">
+                <label for="name">Title:</label>
+                <input 
+                  id="title" 
+                  type="text" 
+                  v-model="product.title"
+                  :disabled="action == 'show-product' ? true : false"
+                  required
+                  class="form-control"
+                />                
+              </div>
+              <!-- Description -->
+              <div class="form-group text-left">
+                <label for="description">Description:</label>
+                <textarea 
+                  v-model="product.description"
+                  :disabled="action == 'show-product' ? true : false"
+                  required
+                  class="form-control"
+                >
+                </textarea>
+              </div>
+              <!-- Image -->
+              <div class="form-group text-left">
+                <label for="image">Image</label>
+                <input 
+                  type="file"
+                  :disable="action == 'show-product' ? true : false"
+                  class="form-control mt-2"
+                />
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button
+                  class="modal-default-button btn btn-outline-success"
+                  @click="$emit('close')"
+              >
+                Close
+              </button>
+              <button
+                  v-if="action != 'show-product'"
+                  class="modal-default-button btn btn-outline-primary"
+                  @click="onHandle"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "Dialog",
+    props: {
+      product: {
+        default: null,
+        type: Object
+      },
+      action: {
+        default : null,
+        type : String
+      }
+    },
+    methods: {
+      //edit product
+      editProduct() {
+        const config = {
+          headers: { "content-type": "multipart/form-data" },
+        };
+        let formData = new FormData();
+        formData.append("title", this.product.title);
+        formData.append("description", this.product.description);
+        formData.append("_method", "PUT");
+
+        let uri = "http://127.0.0.1:8000/products/" + this.product.id;
+        console.log(uri);
+        axios.post(uri, formData, config).then((response) => {
+          console.log(response);
+        });
+
+      },
+      //add product
+      addProduct() {
+
+      },
+      onHandle() {        
+        if (this.action == 'edit-product') {
+          this.editProduct();
+        } else if (this.action == 'add-product') {
+          this.addProduct();
+        }
+      },
+    }
+}
+</script>
+
+
+<style>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 60%;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
--- training-vue/src/components/ProductList.vue
+++ training-vue/src/components/ProductList.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="container">
-    <h3 class="p-3 text-center">List of products</h3>    
+    <h3 class="p-3 text-center">List of products</h3>  
+
+    <Dialog
+      v-if="dialog.showModal"
+      @close="dialog.showModal = false"
+      :product="dialog.product"
+      :action="dialog.action"
+    >
+      <h3 slot="header" v-if="dialog.action === 'add-product'">Add product</h3>
+      <h3 slot="header" v-if="dialog.action === 'edit-product'">Edit product</h3>
+      <h3 slot="header" v-if="dialog.action === 'show-product'">Show product</h3>
+
+    </Dialog>
+
+    <button class="btn btn-info" @click="openDialog({}, 'add-product')"> 
+        <i class="fa fa-plus"></i>
+    </button>
+
     <Pagination v-bind:counter.sync="counter"/>
     <table class="table table-striped table-bordered">
       <thead>
@@ -20,9 +37,23 @@
           <img v-else class="product-image" :src="`http://127.0.0.1:8000/upload/nothumb.jpg`" />
         </td>
         <td>
-          <i class="fa fa-pencil-square-o" aria-hidden="true"></i>&nbsp;
-          <i class="fa fa-trash" aria-hidden="true"></i>&nbsp;
-          <i class="fa fa-clone" aria-hidden="true"></i>
+          <!-- View -->
+          <a            
+            href="#"
+            @click="openDialog(product, 'show-product')"
+          >
+            <i class="fa fa-eye" aria-hidden="true"></i>&nbsp;
+          </a>
+          <!-- Edit -->
+          <a
+            href="#"
+            @click="openDialog(product, 'edit-product')"
+          >
+            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>&nbsp;
+          </a>
+          <!-- Delete -->                    
+          <i class="fa fa-trash" aria-hidden="true"></i>
+          
         </td>
       </tr>
       </tbody>
@@ -34,18 +65,29 @@
 import axios from "axios";
 import {EventBus} from '../main';
 import Pagination from './Pagination.vue';
-
+import Dialog from "./Dialog.vue";
 export default {
-  components: { Pagination },
+  components: { Pagination, Dialog },
   name: 'products',
   data() {
     return {
+      dialog : {
+        showModal : false,
+        action: 'add-product',
+        product: {},
+      },      
       products: [],      
       sub_products: [],
       counter: 1    
     }
   },
   methods: {
+    openDialog(product, action) {
+      console.log('opendialog');
+      this.dialog.product = product;
+      this.dialog.action = action;
+      this.dialog.showModal = true;
+    },
     
     getProducts() {
       let uri = "http://127.0.0.1:8000/products";
@@ -65,8 +107,7 @@ export default {
         this.sub_products = _products.sub_products;
         //this.counter = _products.counter;
       })
-    }
-
+    },     
   },
   created() {        
     this.getProducts();
