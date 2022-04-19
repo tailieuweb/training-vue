34412caf50ded36eb2d3371abc20fa36ae56c70e
tailileuweb <tailieuweb.com@gmail.com>
2022-04-19 13:53:18 +0700

    test

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,12 +1,12 @@
 <!-- https://getbootstrap.com/docs/4.0/components/modal/ -->
 <template>
   <div>
-      <transition name="modal">
+    <transition name="modal">
       <div class="modal-mask">
         <div class="modal-wrapper">
           <!-- Form element -->
           <div class="modal-container">
-            
+
             <div class="modal-header">
               <slot name="header">
                 default header
@@ -14,56 +14,37 @@
             </div>
 
             <div class="dialog-message">
-                {{message}}
+              {{ message }}
             </div>
 
             <div class="modal-body">
               <!-- Title -->
               <div class="form-group text-left">
                 <label for="name">Title:</label>
-                <input 
-                  id="title" 
-                  type="text" 
-                  v-model="product.title"
-                  :disabled="action == 'show-product' ? true : false"
-                  required
-                  class="form-control"
-                />                
+                <input id="title" type="text" v-model="product.title"
+                  :disabled="action == 'show-product' ? true : false" required class="form-control" />
               </div>
               <!-- Description -->
               <div class="form-group text-left">
                 <label for="description">Description:</label>
-                <textarea 
-                  v-model="product.description"
-                  :disabled="action == 'show-product' ? true : false"
-                  required
-                  class="form-control"
-                >
+                <textarea v-model="product.description" :disabled="action == 'show-product' ? true : false" required
+                  class="form-control">
                 </textarea>
               </div>
               <!-- Image -->
               <div class="form-group text-left">
                 <label for="image">Image</label>
-                <input 
-                  type="file"
-                  :disable="action == 'show-product' ? true : false"
-                  class="form-control mt-2"
-                />
+                <input type="file" :disable="action == 'show-product' ? true : false" class="form-control mt-2"
+                  v-on:change="onFileChange" />
               </div>
             </div>
 
             <div class="modal-footer">
-              <button
-                  class="modal-default-button btn btn-outline-success"
-                  @click="$emit('close')"
-              >
+              <button class="modal-default-button btn btn-outline-success" @click="$emit('close')">
                 Close
               </button>
-              <button
-                  v-if="action != 'show-product'"
-                  class="modal-default-button btn btn-outline-primary"
-                  @click="onHandle"
-              >
+              <button v-if="action != 'show-product'" class="modal-default-button btn btn-outline-primary"
+                @click="onHandle">
                 Save
               </button>
             </div>
@@ -77,59 +58,84 @@
 <script>
 import axios from "axios";
 export default {
-    name: "Dialog",
-    data() {
-      return {
-        message: {
-          default: null,
-          type: String
-        }
-      }      
-    },
-    props: {
-      product: {
+  name: "Dialog",
+  data() {
+    return {
+      message: {
         default: null,
-        type: Object
+        type: String
       },
-      action: {
-        default : null,
-        type : String
+      file: null
+    }
+  },
+  props: {
+    product: {
+      default: null,
+      type: Object
+    },
+    action: {
+      default: null,
+      type: String
+    }
+  },
+  methods: {
+    //edit product
+    editProduct() {
+      const config = {
+        headers: { "content-type": "multipart/form-data" },
+      };
+      let formData = new FormData();
+      formData.append("title", this.product.title);
+      formData.append("description", this.product.description);
+      formData.append("image", this.file);
+
+
+      let uri = "http://127.0.0.1:8000/products/" + this.product.id;
+      console.log(uri);
+      axios.post(uri, formData, config).then((response) => {
+        if (response.data.success) {
+          this.message = 'Update successful';
+        } else {
+          this.message = 'Update failed';
+        }
+      });
+
+    },
+    //add product
+    addProduct() {
+      const config = {
+        headers: { "content-type": "multipart/form-data" },
+      };
+      let formData = new FormData();
+      formData.append("title", this.product.title);
+      formData.append("description", this.product.description);
+      formData.append("image", this.file);
+
+
+      let uri = "http://127.0.0.1:8000/products";
+      console.log(uri);
+      axios.post(uri, formData, config).then((response) => {
+        if (response.data.success) {
+          this.message = 'Add successful';
+        } else {
+          this.message = 'Add failed';
+        }
+      });
+
+    },
+    onHandle() {
+      if (this.action == 'edit-product') {
+        this.editProduct();
+      } else if (this.action == 'add-product') {
+        this.addProduct();
       }
     },
-    methods: {
-      //edit product
-      editProduct() {
-        const config = {
-          headers: { "content-type": "multipart/form-data" },
-        };
-        let formData = new FormData();
-        formData.append("title", this.product.title);
-        formData.append("description", this.product.description);
+    onFileChange(e) {
+      console.log(e.target.files[0]);
+      this.file = e.target.files[0];
+    },
 
-
-        let uri = "http://127.0.0.1:8000/products/" + this.product.id ;
-        console.log(uri);
-        axios.post(uri, formData, config).then((response) => {
-          if (response.data.success) {
-            this.message = 'Update successful';
-          } else {
-            this.message = 'Update failed';
-          }
-        });
-
-      },
-      //add product
-      addProduct() {
-
-      },
-      onHandle() {        
-        if (this.action == 'edit-product') {
-          this.editProduct();
-        } else if (this.action == 'add-product') {
-          this.addProduct();
-        }
-      },
-    }
+  }
 }
 </script>
 
